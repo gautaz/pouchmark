37df4310936277020abcaeab98c4ae0541f13364
Emmanuel <emmanuel.lefebvre@gmail.com>
2015-08-09 21:55:34 +0000

    Added general balance

--- www/pouchmark.js
+++ www/pouchmark.js
@@ -1,15 +1,15 @@
-(function () {
+(function() {
 	/* global chance, PouchDB, Promise */
 	'use strict';
 
-	var getParam = function (name) {
+	var getParam = function(name) {
 		name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
 		var regex = new RegExp('[\\?&]' + name + '=([^&#]*)'),
 			results = regex.exec(location.search);
 		return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
 	};
 
-	var createDoc = function () {
+	var createDoc = function() {
 		return {
 			_id: chance.guid(),
 			date: chance.date({
@@ -23,24 +23,53 @@
 	};
 
 	window.PouchDB = PouchDB;
-	var db = new PouchDB('benchmark', { size: 100 });
+	var db = new PouchDB('benchmark', {
+		size: 100
+	});
 	var puts = [];
 	var ids = [];
 	var start = new Date();
 	var numDocs = parseInt(getParam('num'), 10) || 100;
 	var index;
+	var sum = 0.0;
 
 	console.log('put');
 	for (index = 0; index < numDocs; ++index) {
 		var doc = createDoc();
 		ids.push(doc._id);
+		sum += doc.amount;
 
 		// puts.push(db.put(doc));
 		puts.push(doc);
 	}
 
+	console.log("Sum init : " + sum);
+
+	var ddoc = {
+		_id: '_design/my_index',
+		views: {
+			by_name: {
+				map: function(doc) {
+					emit(doc.date, doc.amount);
+				}.toString(),
+				reduce: '_sum'
+			}
+		}
+	};
+
+	// save it
+	db.put(ddoc).then(function() {
+		console.log("Design index added");
+	}).catch(function(err) {
+		// some error (maybe a 409, because it already exists?)
+		console.log(err);
+		if (err.status !== 409) {
+			throw err;
+		}
+	});
+
 	// db.bulkDocs(puts);
-	document.addEventListener('DOMContentLoaded', function () {
+	document.addEventListener('DOMContentLoaded', function() {
 		document.getElementById('num').innerHTML = numDocs;
 		start = new Date();
 
@@ -56,9 +85,29 @@
 
 			console.log('bulk get include docs');
 			start = new Date();
-			return db.allDocs({ include_docs: true });
+			return db.allDocs({
+				include_docs: true
+			});
 		}).then(function(result) {
 			document.getElementById('ms_bulk_get_include_docs').innerHTML = new Date() - start;
+			console.log('map/reduce for general balance');
+
+			start = new Date();
+			return db.query('my_index/by_name', {
+				reduce: '_sum'
+			});
+		}).then(function(result) {
+			document.getElementById('ms_mapreduce').innerHTML = new Date() - start;
+			console.log('map/reduce for general balance 2 second');
+
+			start = new Date();
+			return db.query('my_index/by_name', {
+				reduce: true
+			});
+		}).then(function(result) {
+			console.log("Balance with map/reduce : " + JSON.stringify(result));
+			document.getElementById('ms_mapreduce2').innerHTML = new Date() - start;
+
 			console.log('remove');
 			var elements = result.rows;
 			var removes = [];
@@ -66,7 +115,7 @@
 			for (index = 0; elements < elements.length; ++index) {
 				removes.push(db.remove(elements[index]));
 			}
-			
+
 			return Promise.all(removes);
 		}).then(function() {
 			document.getElementById('ms_delete').innerHTML = new Date() - start;
@@ -83,33 +132,33 @@
 	});
 })();
 
-		// .then(function() {
-		// 	document.getElementById('ms_bulk_get').innerHTML = new Date() - start;
+// .then(function() {
+// 	document.getElementById('ms_bulk_get').innerHTML = new Date() - start;
 
-		// 	console.log('get');
-		// 	start = new Date();
-		// 	var gets = [];
-		// 	for (index = 0; index < ids.length; ++index) {
-		// 		gets.push(db.get(ids[index]));
-		// 	}
-		// 	return Promise.all(gets);
-		// })
-		// .then(function(getArray) {
-		// 	document.getElementById('ms_get').innerHTML = new Date() - start;
+// 	console.log('get');
+// 	start = new Date();
+// 	var gets = [];
+// 	for (index = 0; index < ids.length; ++index) {
+// 		gets.push(db.get(ids[index]));
+// 	}
+// 	return Promise.all(gets);
+// })
+// .then(function(getArray) {
+// 	document.getElementById('ms_get').innerHTML = new Date() - start;
 
-		// 	console.log('updates');
-		// 	start = new Date();
+// 	console.log('updates');
+// 	start = new Date();
 
-		// 	var updates = [];
-		// 	for (index = 0; index < getArray.length; ++index) {
-		// 		updates.push(db.put({
-		// 			_id: getArray[index]._id,
-		// 			_rev: getArray[index]._rev,
-		// 			amount: Math.floor(Math.random() * 100000 - 50000) / 100,
-		// 		}));
-		// 	}
-		// 	return Promise.all(updates);
-		// }).then(function(updateArray) {
-		// 	document.getElementById('ms_update').innerHTML = new Date() - start;
-		// 	return db.allDocs();
-		// })
+// 	var updates = [];
+// 	for (index = 0; index < getArray.length; ++index) {
+// 		updates.push(db.put({
+// 			_id: getArray[index]._id,
+// 			_rev: getArray[index]._rev,
+// 			amount: Math.floor(Math.random() * 100000 - 50000) / 100,
+// 		}));
+// 	}
+// 	return Promise.all(updates);
+// }).then(function(updateArray) {
+// 	document.getElementById('ms_update').innerHTML = new Date() - start;
+// 	return db.allDocs();
+// })
